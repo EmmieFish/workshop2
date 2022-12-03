ca5b35f30b8ea7a1ea8c0204c5c5164b4f1aefc0
MaryElise Collier <maryelisecollier@gmail.com>
2022-12-03 10:23:28 -0800

    first commint

---
+++ data/misc/django_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS nc_tutorials_db;
+CREATE DATABASE nc_tutorials_db;
+
+\c nc_tutorials_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
